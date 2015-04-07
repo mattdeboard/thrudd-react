c3f99d49f5f7c9e6de6921779b12f44ceaf105fe
Matt DeBoard <matt.deboard@gmail.com>
2015-04-07 01:12:56 -0500

    Add some primitive paging buttons.

---
+++ src/js/components/PageButton.jsx
@@ -0,0 +1,33 @@
+const React = require('react');
+const classnames = require('classnames');
+
+let PageButton = React.createClass({
+  propTypes: {
+    clickHandler: React.PropTypes.func.isRequired,
+    direction: React.PropTypes.oneOf([1, -1]).isRequired
+  },
+
+  handleClick: function(e) {
+    return this.props.clickHandler(e, this.props.direction);
+  },
+
+  render: function() {
+    let buttonText = this.props.direction == 1 ? "Next" : "Previous";
+    // Obviously this is pretty rudimentary styling, but it gets the
+    // job done.
+    let classNames = classnames(
+      "btn",
+      "btn-default"
+    );
+    return (
+      <button
+        className={classNames}
+        type="button"
+        onClick={this.handleClick}>
+        {`${buttonText} Page`}
+      </button>
+    );
+  }
+});
+
+module.exports = PageButton;
--- src/js/components/SearchResultsView.jsx
+++ src/js/components/SearchResultsView.jsx
@@ -1,8 +1,10 @@
 const React = require('react');
 const fs = require('fs');
 const Immutable = require('immutable');
+const classnames = require('classnames');
 const SearchResultStore = require('../stores/SearchResultStore');
 const SearchResult = require('./SearchResult.jsx');
+const PageButton = require('./PageButton.jsx');
 const { addSearchResult } = require('../actions/ActionCreators');
 const { SEARCH_RESULT_PAGE_SIZE } = require('../constants/AppConstants');
 
@@ -46,12 +48,24 @@ let SearchResultsView = React.createClass({
     };
   },
 
+  handlePageButtonClick: function(e, direction) {
+    let targetPage = this.state.currentPage + direction;
+    if (targetPage < 0) {
+      targetPage = 0;
+    }
+    let start = SEARCH_RESULT_PAGE_SIZE * targetPage;
+    let end = start + SEARCH_RESULT_PAGE_SIZE;
+    this.setState({
+      results: SearchResultStore.getPage(start, end),
+      currentPage: targetPage
+    });
+  },
+
   handleSearchResultChange: function(e) {
     let start = SEARCH_RESULT_PAGE_SIZE * this.state.currentPage;
     let end = start + SEARCH_RESULT_PAGE_SIZE;
     return this.setState({
-      results: SearchResultStore.getPage(start, end),
-      currentPage: this.state.currentPage + 1
+      results: SearchResultStore.getPage(start, end)
     });
   },
 
@@ -72,7 +86,23 @@ let SearchResultsView = React.createClass({
     });
   },
 
+  renderPageButtons: function() {
+    const clickHandler = this.handlePageButtonClick;
+    return [1, -1].map(function(i) {
+      return (
+        <PageButton
+          clickHandler={clickHandler}
+          direction={i} />
+      );
+    });
+  },
+
   render: function() {
+    const resultCount = SearchResultStore.getResultCount();
+    let pages = Math.floor(resultCount / SEARCH_RESULT_PAGE_SIZE);
+    if (resultCount % SEARCH_RESULT_PAGE_SIZE) {
+      pages += 1;
+    }
     return (
       <div id="DataListing">
         <hr />
@@ -82,6 +112,13 @@ let SearchResultsView = React.createClass({
             {this.renderResultRows()}
           </tbody>
         </table>
+        <div className="row col-md-6 col-md-offset-6">
+          <PageButton clickHandler={this.handlePageButtonClick} direction={1}/>
+          <p className="lead">
+            {`Page ${this.state.currentPage} of ${pages}`}
+          </p>
+          <PageButton clickHandler={this.handlePageButtonClick} direction={-1}/>
+        </div>
       </div>
     );
   }
